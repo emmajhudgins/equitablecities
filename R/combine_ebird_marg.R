e63adb36d0ae9ba6e1135f7cf00a89d1644a4003
Emma Hudgins <emma.hudgins@mail.mcgill.ca>
2021-06-28 18:45:07 -0400

    updated analyses

--- R/combine_ebird_marg.R
+++ R/combine_ebird_marg.R
@@ -1,4 +1,4 @@
-# determine % of DBUIDs covered by >=17 eBird checklists
+# Combine eBird bird diversity data with CANUE marginalization indices
 # written by Emma J. Hudgins April 16, 2021
 # for questions email emma.hudgins@carleton.ca
 
@@ -15,87 +15,140 @@ setwd(here())
 
 marg<-read.csv('../raw/cmg_a_2021-05-11_18-54-04_annual/cmg_a_06.csv')#marginalization indices from CANUE downloaded May 11th,2021
 colnames(marg)[1]<-"POSTALCODE"
+poly <- readOGR("../raw/lcma000b16a_e/lcma000b16a_e.shp") #2016 CMA and CA boundaries from Canadian Census (Statistics Canada, Downloaded April 14)
+poly<-subset(poly, CMATYPE=="B") # only CMAs
+
+
 files<-list.files('../data/')
 files<-files[grepl("CMA_summary_",files)]
-all_birddiv<-tibble(POSTALCODE=character(), year=numeric(), n_checklists=numeric(), species_richness=numeric(), lat=numeric(), long=numeric())
+all_birddiv<-tibble(POSTALCODE=character(), year=numeric(), n_checklists=numeric(), species_richness=numeric())
 for (file in files)
 {
   dat<-read.csv(paste0('../data/',file))
   all_birddiv<-add_row(dat, all_birddiv)
 }
 
-cdb_pccf$DBUID<-as.numeric(cdb_pccf$DBUID)
-cdb_pccf_bird<-cdb_pccf%>%left_join(all_birddiv, "DBUID")
-cdb_pccf_bird_marg<-cdb_pccf_bird%>%left_join(marg, "PC")
-saveRDS(cdb_pccf_bird_marg, file="../output/cdb_pccf_bird.RDS")
-#free up some memory
-rm(cdb_pccf_bird)
-rm(cdb_pccf)
-rm(pccf)
-df<-subset(cdb_pccf_bird_marg, n_checklists>=17)#threshold for sufficient effort (only ~10%)
+marg_bird<-all_birddiv%>%left_join(marg, "POSTALCODE")
+saveRDS(marg_bird, file="../output/marg_bird.RDS")
+
+
+df<-subset(marg_bird, n_checklists>=17)#threshold for sufficient effort (only ~10%)
 df$cmg06_10[which(df$cmg06_10==-9999)]<-NA
 df$cmg06_11[which(df$cmg06_11==-9999)]<-NA
 df$cmg06_12[which(df$cmg06_12==-9999)]<-NA
 df$cmg06_13[which(df$cmg06_13==-9999)]<-NA
+
 
 library(mgcv)
 m<-gam(log(df$species_richness+1)~cmg06_10+cmg06_11+cmg06_12+cmg06_13+s(year), data=df)#instability, deprivation, dependency, ethnic concentration
 summary(m)
 library(viridis)
 pal<-viridis
+poly_all<-poly
 poly<-poly[,c("CMAUID")]
 library(raster)
-provinces <- getData(country="Canada", level=1)
+provinces <- raster::getData(country="Canada", level=1)
 provinces<-subset(provinces, ENGTYPE_1!="Territory")
 crs(provinces)<-"+proj=longlat +datum=WGS84"
-e <- as(extent(poly), "SpatialPolygons")
-crs(e)<- "+proj=lcc +lat_0=63.390675 +lon_0=-91.8666666666667 +lat_1=49 +lat_2=77 +x_0=6200000 +y_0=3000000 +datum=NAD83 +units=m +no_defs"
-# provinces<-spTransform(provinces,CRS("+proj=lcc +lat_0=63.390675 +lon_0=-91.8666666666667 +lat_1=49 +lat_2=77 +x_0=6200000 +y_0=3000000 +datum=NAD83 +units=m +no_defs"))
-# provinces<-crop(provinces, e)
-bins<-seq(0,max((df$species_richness+1)),length.out=50)
-colours<-pal(50)[findInterval(df$species_richness, bins)+1]
+# e <- as(extent(poly), "SpatialPolygons")
+# crs(e)<- "+proj=lcc +lat_0=63.390675 +lon_0=-91.8666666666667 +lat_1=49 +lat_2=77 +x_0=6200000 +y_0=3000000 +datum=NAD83 +units=m +no_defs"
+poly<-spTransform(poly,CRS("+proj=longlat +datum=WGS84"))
+#provinces<-crop(provinces, e)
+df_postal<-df%>%group_by(POSTALCODE)%>%summarize_if(is.numeric, mean, na.rm=T)
+df_postal<-subset(df_postal, is.nan(latitude)==F)
+coordinates(df_postal)<-~longitude+latitude
+crs(df_postal)<-"+proj=longlat +datum=WGS84"
+for (i in 1:nrow(poly))
+{
+  
+  within<-which(over(df_postal,poly[i,])!='<NA>')
+  crop_provinces<-crop(provinces, df_postal[within,])
+  bins<-seq(0,max((df$species_richness+1)),length.out=50)
+  colours<-pal(50)[findInterval(df$species_richness, bins)+1]
+  
+pdf(paste0('../plots/bird_richness_',gsub("\\/.*", "",poly_all$CMANAME[i]),'.pdf'))
+par(mar=c(0.5,0.5,0.5,0.5))
+par(oma=c(0,0,0,4))
+layout(t(1:2), widths=c(6,1))
+plot(crop_provinces, main=gsub("\\/.*", "",poly_all$CMANAME[i]))
+plot(poly[i,], add=T)
+points(cbind(df_postal$longitude, df_postal$latitude)[within,], pch=15, cex=2,col=colours)
+image(1,1:50,t(1:50), col=pal(50), axes=FALSE)
+par(las=1)
+axis(4,labels=c("lowest", "highest"),at=c(1,50), cex.axis=1)
+par(las=0)
+mtext(side=4, "Bird Richness", line=3)
 
-pdf('../plots/bird_richness.pdf')
-plot(provinces)
-plot(poly, add=T)
-points(cbind(df$LONG, df$LAT), pch=15, cex=0.05, col=colours)
 dev.off()
 
 
 bins<-seq(min(df$cmg06_11, na.rm=T),max(df$cmg06_11, na.rm=T),length.out=50)
 colours<-pal(50)[findInterval(df$cmg06_11, bins)+1]
 
-pdf('../plots/Deprivation.pdf')
-plot(provinces)
-plot(poly, main='Deprivation', add=T)
-points(cbind(df$LONG, df$LAT), pch=15, cex=0.05, col=colours)
+pdf(paste0('../plots/Deprivation_',gsub("\\/.*", "",poly_all$CMANAME[i]),'.pdf'))
+par(mar=c(0.5,0.5,0.5,0.5))
+par(oma=c(0,0,0,4))
+layout(t(1:2), widths=c(6,1))
+plot(crop_provinces)
+plot(poly, main=gsub("\\/.*", "",poly_all$CMANAME[i]), add=T)
+points(cbind(df_postal$longitude, df_postal$latitude)[within,], pch=15, cex=2,col=colours)
+image(1,1:50,t(1:50), col=pal(50), axes=FALSE)
+par(las=1)
+axis(4,labels=c("lowest", "highest"),at=c(1,50), cex.axis=1)
+par(las=0)
+mtext(side=4, "Deprivation", line=3)
 dev.off()
 
 bins<-seq(min(df$cmg06_13, na.rm=T),max(df$cmg06_13, na.rm=T),length.out=50)
 colours<-pal(50)[findInterval(df$cmg06_13, bins)+1]
 
-pdf('../plots/Ethnic Concentration.pdf')
-plot(provinces)
-plot(poly, main='Ethnic Concentration', add=T)
-points(cbind(df$LONG, df$LAT), pch=15, cex=0.05, col=colours)
+pdf(paste0('../plots/Ethnic_concentration_',gsub("\\/.*", "",poly_all$CMANAME[i]),'.pdf'))
+par(mar=c(0.5,0.5,0.5,0.5))
+par(oma=c(0,0,0,4))
+layout(t(1:2), widths=c(6,1))
+plot(crop_provinces,main=gsub("\\/.*", "",poly_all$CMANAME[i]))
+plot(poly, add=T)
+points(cbind(df_postal$longitude, df_postal$latitude)[within,], pch=15, cex=2,col=colours)
+image(1,1:50,t(1:50), col=pal(50), axes=FALSE)
+par(las=1)
+axis(4,labels=c("lowest", "highest"),at=c(1,50), cex.axis=1)
+par(las=0)
+mtext(side=4, "Ethnic concentration", line=3)
 dev.off()
 
 
 bins<-seq(min(df$cmg06_10, na.rm=T),max(df$cmg06_10, na.rm=T),length.out=50)
 colours<-pal(50)[findInterval(df$cmg06_10, bins)+1]
 
-pdf('../plots/instability.pdf')
-plot(provinces)
-plot(poly, main='Instability', add=T)
-points(cbind(df$LONG, df$LAT), pch=15, cex=0.05, col=colours)
+pdf(paste0('../plots/Instability',gsub("\\/.*", "",poly_all$CMANAME[i]),'.pdf'))
+par(mar=c(0.5,0.5,0.5,0.5))
+par(oma=c(0,0,0,4))
+layout(t(1:2), widths=c(6,1))
+plot(crop_provinces, main=gsub("\\/.*", "",poly_all$CMANAME[i]))
+plot(poly, add=T)
+points(cbind(df_postal$longitude, df_postal$latitude)[within,], pch=15, cex=2,col=colours)
+image(1,1:50,t(1:50), col=pal(50), axes=FALSE)
+par(las=1)
+axis(4,labels=c("lowest", "highest"),at=c(1,50), cex.axis=1)
+par(las=0)
+mtext(side=4, "Instability", line=3)
 dev.off()
 
 
 bins<-seq(min(df$cmg06_12, na.rm=T),max(df$cmg06_12, na.rm=T),length.out=50)
 colours<-pal(50)[findInterval(df$cmg06_12,bins)+1]
 
-pdf('../plots/Dependency.pdf')
-plot(provinces)
-plot(poly, main='Dependency', add=T)
-points(cbind(df$LONG, df$LAT), pch=15, cex=0.05, col=colours)
+pdf(paste0('../plots/Dependency_',gsub("\\/.*", "",poly_all$CMANAME[i]),'.pdf'))
+par(mar=c(0.5,0.5,0.5,0.5))
+par(oma=c(0,0,0,4))
+layout(t(1:2), widths=c(6,1))
+plot(crop_provinces, main=gsub("\\/.*", "",poly_all$CMANAME[i]))
+plot(poly, add=T)
+points(cbind(df_postal$longitude, df_postal$latitude)[within,], pch=15, cex=2,col=colours)
+image(1,1:50,t(1:50), col=pal(50), axes=FALSE)
+par(las=1)
+axis(4,labels=c("lowest", "highest"),at=c(1,50), cex.axis=1)
+par(las=0)
+mtext(side=4, "Dependency", line=3)
 dev.off()
+}
--- R/completeness_by_CMA.R
+++ R/completeness_by_CMA.R
@@ -1,4 +1,4 @@
-# determine % of DBUIDs covered by >=17 eBird checklists
+# determine % and number of Postal codes in a given CMA covered by >=17 eBird checklists
 # written by Emma J. Hudgins April 16, 2021
 # for questions email emma.hudgins@carleton.ca
 
@@ -13,18 +13,16 @@ setwd(here())
 poly <- read_sf("../raw/lcma000b16a_e/lcma000b16a_e.shp") #2016 CMA and CA boundaries from Canadian Census (Statistics Canada, Downloaded April 14)
 poly<-subset(poly, CMATYPE=="B") # only CMAs
 write_sf(poly, '../data/CMA_shp.shp')
-cdb<- read_sf('../raw/db_000b16a_e/ldb_000b16a_e.shp') #census dissemination block (smallest unit)
-cdb<-subset(cdb, CMATYPE=="B") #census dissemination block (smallest 
 
 data_db<-list()
 completeness<-list()
 for (i in 1:nrow(poly))
 {
-  cdb_sub<-subset(cdb, CMANAME==poly$CMANAME[i])
-  data_db<-read.csv(paste0("CMA_summary_", gsub("\\/.*", "",poly$CMANAME[i]),".csv"))
+  postal_CMA<-readRDS(paste0('../data/postal_CMA_',gsub("\\/.*", "",poly$CMANAME[i]), ".RDS"))
+  data_db<-read.csv(paste0("../data/CMA_summary_", gsub("\\/.*", "",poly$CMANAME[i]),".csv"))
   data_db<-subset(data_db, n_checklists>=17)
-  completeness[[i]]<-data_db%>%group_by(year)%>%summarise_at('DBUID', n_distinct)
-  completeness[[i]]$percent<-completeness[[i]]$DBUID/length(unique(cdb_sub$DBUID))
+  completeness[[i]]<-data_db%>%group_by(year)%>%summarise_at('POSTALCODE', n_distinct)
+  completeness[[i]]$percent<-completeness[[i]]$POSTALCODE/length(unique(postal_CMA$POSTALCODE))
 }
 
 saveRDS(completeness, file="../output/completeness_by_CMA.RDS")
